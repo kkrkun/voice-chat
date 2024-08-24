a794882750fb7f893e68dbbdf197fe5e21213be4
kkrkun <110749983+kkrkun@users.noreply.github.com>
2024-08-24 21:17:47 +0900

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ window.onload = async function () {
 };
 
 let serverDistance = 7; // デフォルトの distance
-let socket = null;
 
 const Token = new SkyWayAuthToken({
     jti: uuidV4(),
@@ -54,8 +53,6 @@ async function establishWebSocketConnection() {
     const url = 'wss://kkryade1212.tcpexposer.com';
     let socket;
 
-    const retryInterval = 5000; // 5秒ごとに再試行
-
     const connect = () => {
         return new Promise((resolve, reject) => {
             socket = new WebSocket(url);
@@ -72,10 +69,7 @@ async function establishWebSocketConnection() {
 
             socket.addEventListener('close', () => {
                 console.log('WebSocket connection closed');
-                setTimeout(() => {
-                    console.log('Retrying WebSocket connection...');
-                    connect().then(resolve).catch(reject);
-                }, retryInterval);
+                // 再接続しないため、ここでの再接続ロジックを削除
             });
         });
     };
@@ -88,6 +82,7 @@ async function establishWebSocketConnection() {
         throw error; // 必要に応じてエラー処理
     }
 }
+
 
 async function SkyWay_main(token) {
     const { SkyWayContext, SkyWayRoom, SkyWayStreamFactory } = skyway_room;
@@ -118,40 +113,43 @@ async function SkyWay_main(token) {
             alert('名前を入力してください');
             return;
         }
-
+    
+        // まずWebSocket接続を確立する
+        const socket = await establishWebSocketConnection();
+    
         const audio = await SkyWayStreamFactory.createMicrophoneAudioStream();
-
+    
         if (roomNameInput === '') return;
-
+    
         const context = await SkyWayContext.Create(token);
         const room = await SkyWayRoom.FindOrCreate(context, {
             type: 'p2p',
             name: roomNameInput,
         });
         const me = await room.join({ name: userName });
-
+    
         const publication = await me.publish(audio);
-
+    
         console.log(`${userName} is connected`);
-
+    
         target.textContent = "ミュート解除中";
         NonMutebtn.style.backgroundColor = "rgb(147, 235, 235)";
-
+    
         myId.textContent = me.id;
         myName.textContent = userName;
         Memberselem.textContent = Members + "人";
         IdDisp.style.visibility = "visible";
-
+    
         NonMutebtn.style.visibility = "visible";
         NonMutebtn.style.opacity = 1;
         joinButton.style.visibility = "hidden";
         leavebtn.style.visibility = "visible";
-
+    
         leavebtn.onclick = () => {
             me.leave();
             location.reload();
         };
-
+    
         NonMutebtn.addEventListener('click', async () => {
             isMuted = !isMuted;
             if (isMuted) {
@@ -164,23 +162,23 @@ async function SkyWay_main(token) {
                 await publication.enable();
             }
         });
-
+    
         const subscribeAndAttach = async (publication) => {
             if (publication.publisher.id === me.id) return;
-
+    
             const subscribeButton = document.createElement('button');
             subscribeButton.textContent = `${publication.publisher.name || publication.publisher.id}: ${publication.contentType}`;
             buttonArea.appendChild(subscribeButton);
-
+    
             subscribeButton.onclick = async () => {
                 try {
                     const { stream } = await me.subscribe(publication.id);
-
+    
                     const oldMediaElement = remoteMediaArea.querySelector(`[data-username="${publication.publisher.name || publication.publisher.id}"]`);
                     if (oldMediaElement) {
                         remoteMediaArea.removeChild(oldMediaElement);
                     }
-
+    
                     let newMedia;
                     switch (stream.track.kind) {
                         case 'audio':
@@ -195,31 +193,41 @@ async function SkyWay_main(token) {
                     }
                     stream.attach(newMedia);
                     remoteMediaArea.appendChild(newMedia);
-
-                    // WebSocket接続の確認とメッセージ処理の設定
-                    if (!socket || socket.readyState !== WebSocket.OPEN) {
-                        try {
-                            socket = await establishWebSocketConnection();
-                        } catch (error) {
-                            console.error('Failed to establish WebSocket connection:', error);
-                            return;
-                        }
-                    }
-
+    
+                    // WebSocket接続の処理
                     socket.addEventListener('message', (event) => {
-                        handleWebSocketMessage(event, userPositions, myName.textContent);
+                        console.log('Received data:', event.data);
+                        const data = JSON.parse(event.data);
+                        const positions = data.positions;
+                        serverDistance = data.distance; // サーバーから受け取った distance を使用
+                        console.log(serverDistance);
+                        for (const [name, position] of Object.entries(positions)) {
+                            if (!userPositions[name]) {
+                                userPositions[name] = { x: 0, y: 10000, z: 0 };
+                            } else if (!position || Object.keys(position).length === 0) {
+                                userPositions[name] = { x: 0, y: 10000, z: 0 };
+                            } else {
+                                userPositions[name] = position;
+                            }
+    
+                            const mediaElement = document.querySelector(`[data-username="${name}"]`);
+                            if (name != myName.textContent && mediaElement && userPositions[myName.textContent] && userPositions[name] && position && Object.keys(position).length >= 1) {
+                                console.log(`Adjusting volume for ${name}:`, userPositions[myName.textContent], userPositions[name]);
+                                adjustVolume(mediaElement, userPositions[myName.textContent], userPositions[name]);
+                            }
+                        }
                     });
-
+    
                 } catch (error) {
                     console.error('Failed to subscribe to publication:', error);
                 }
             };
-
+    
             subscribeButton.click();
             Members++;
             Memberselem.textContent = Members + "人";
         };
-
+    
         room.onStreamPublished.add((e) => {
             console.log('New publication:', e.publication);
             subscribeAndAttach(e.publication);
@@ -228,51 +236,52 @@ async function SkyWay_main(token) {
             Members--;
             Memberselem.textContent = Members + "人";
         });
-
+    
         room.publications.forEach(publication => {
             subscribeAndAttach(publication);
         });
-
+    
         await publication.enable();
-    };
-}
-
-function handleWebSocketMessage(event, userPositions, myNameText) {
-    console.log('Received data:', event.data);
-    const data = JSON.parse(event.data);
-    const positions = data.positions;
-    serverDistance = data.distance; // サーバーから受け取った distance を使用
-    for (const [name, position] of Object.entries(positions)) {
-        if (!userPositions[name]) {
-            userPositions[name] = { x: 0, y: 10000, z: 0 };
-        } else if (!position || Object.keys(position).length === 0) {
-            userPositions[name] = { x: 0, y: 10000, z: 0 };
-        } else {
-            userPositions[name] = position;
-        }
-
-        const mediaElement = document.querySelector(`[data-username="${name}"]`);
-        if (name !== myNameText && mediaElement && userPositions[myNameText] && userPositions[name] && position && Object.keys(position).length >= 1) {
-            console.log(`Adjusting volume for ${name}:`, userPositions[myNameText], userPositions[name]);
-            adjustVolume(mediaElement, userPositions[myNameText], userPositions[name]);
-        }
-    }
+    };    
 }
 
 navigator.permissions.query({ name: 'microphone' }).then((result) => {
-    if (result.state === 'denied') {
-        alert("マイクへのアクセスが拒否されました");
-        location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    if (result.state === 'granted') {
+        console.log("マイクを利用します");
+    } else {
+        console.log("マイクの権限取得エラーです");
+        alert("マイクを使用する権限を与えて下さい");
     }
 });
 
-function adjustVolume(audioElement, userPosition, targetPosition) {
-    if (!userPosition || !targetPosition) return;
-    const distance = Math.sqrt(
-        Math.pow(userPosition.x - targetPosition.x, 2) +
-        Math.pow(userPosition.z - targetPosition.z, 2)
+function calculateDistance(pos1, pos2) {
+    return Math.sqrt(
+        Math.pow(pos1.x - pos2.x, 2) +
+        Math.pow(pos1.y - pos2.y, 2) +
+        Math.pow(pos1.z - pos2.z, 2)
     );
-    const volume = Math.max(1 - (distance / serverDistance), 0);
-    audioElement.volume = volume;
-    console.log(`Volume adjusted for ${audioElement.getAttribute('data-username')}: ${volume}`);
+}
+
+function adjustVolume(mediaElement, pos1, pos2) {
+    if (!pos1 || !pos2 || typeof pos1.x !== 'number' || typeof pos1.y !== 'number' || typeof pos1.z !== 'number' ||
+        typeof pos2.x !== 'number' || typeof pos2.y !== 'number' || typeof pos2.z !== 'number') {
+        console.error('Invalid positions:', pos1, pos2);
+        mediaElement.volume = 0;
+        mediaElement.muted = true;
+        return;
+    }
+
+    const distance = calculateDistance(pos1, pos2);
+    const minVolume = 0;
+    const volume = Math.max(minVolume, 1 - (distance / serverDistance)); // serverDistance を使用
+    if (volume == 0) {
+        mediaElement.volume = minVolume;
+        mediaElement.muted = true;
+    } else {
+        mediaElement.volume = volume;
+        mediaElement.muted = false;
+    }
+    console.log(`now volume: ${volume}`);
+    console.log(`Media element volume set to: ${mediaElement.volume}`);
+    console.log(`Volume adjusted for ${mediaElement.getAttribute('data-username')}: ${mediaElement.volume}`);
 }
