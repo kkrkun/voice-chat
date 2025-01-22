878efd7d255748dbf77425f42cf66bbcb5b4e0ae
kkrkun <110749983+kkrkun@users.noreply.github.com>
2025-01-22 23:03:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { nowInSec, SkyWayAuthToken, SkyWayContext, SkyWayRoom, SkyWayStreamFactory, uuidV4 } = skyway_room;
 
-let url; //サーバー側と接続するためのws/// ws to connect with the server side
+let url;
 const slider = {};
 let Members = 0;
 let nowAd;
@@ -8,17 +8,22 @@ const userLang = navigator.language || navigator.userLanguage;
 let lang = userLang.startsWith('ja') ? 'ja' : 'en';
 let isPC;
 document.addEventListener("DOMContentLoaded", function () {
-    // ブラウザの言語設定を取得
-    const userLang = navigator.language || navigator.userLanguage;
-
-    // すべての言語コンテンツを非表示にする
-    const contents = document.querySelectorAll('.language-content');
-    contents.forEach(content => {
-        content.style.display = 'none';
-    });
 
     // 選択した言語のコンテンツを表示
-    document.getElementById(lang).style.display = 'block';
+    if (lang = 'ja') {
+        document.getElementById('my-servers').style.display = 'block';
+        document.getElementById('header-text').textContent = "近接VC in Mcbe";
+        document.getElementById('name-disp').innerHTML = "ゲーマータグ: <span id='my-name'></span>";
+        document.getElementById('Menber-disp').innerHTML = "参加人数: <span id='Members'>不明</span>";
+        document.getElementById('address').textContent = "アドレス: ";
+        document.getElementById('gamertag').textContent = "ゲーマータグ: ";
+        document.getElementById('join').textContent = "接続する";
+        document.getElementById('NonMute-btn').textContent = "ミュートON/OFF";
+        document.getElementById('MuteInfo').textContent = "接続されていません";
+        document.getElementById('leave').textContent = "退出";
+        document.getElementById('participant').textContent = "参加者リスト";
+        document.getElementById('switch-language').textContent = "Switch to English";
+    }
 
     function checkHeight() {
         // 画面幅でPCかスマホを判定
@@ -26,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // PCの場合は日本語用広告を表示
         if (isPC) {
-            if (userLang.startsWith('ja')) {
+            if (lang = 'ja') {
                 document.getElementById('ad-japanese-pc').style.display = 'block';  // 日本語用広告
                 if (window.matchMedia("(min-height: 850px)").matches) {
                     document.getElementById('ad-japanese2-pc').style.display = 'block';  // 日本語用広告
@@ -42,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             }
         } else {
-            if (userLang.startsWith('ja')) {
+            if (lang = 'ja') {
                 document.getElementById('ad-japanese-phone').style.display = 'block';  // 日本語用広告
             } else {
                 document.getElementById('ad-other-phone').style.display = 'block';  // 日本語以外用広告
@@ -57,98 +62,116 @@ document.addEventListener("DOMContentLoaded", function () {
     // 言語切り替えボタンの動作
     const switchButton = document.getElementById('switch-language');
     switchButton.addEventListener('click', function () {
-        // 言語切り替え
-        lang = 'ja';
-        document.getElementById('en').style.display = 'none';
-        document.getElementById('ja').style.display = 'block';
-        if (isPC) {
-            document.getElementById('ad-japanese-pc').style.display = 'block';  // 日本語用広告
-            document.getElementById('ad-japanese2-pc').style.display = 'block';  // 日本語用広告
-            document.getElementById('ad-other-pc').style.display = 'none';  // 日本語以外用広告
-            document.getElementById('ad-other-pc2').style.display = 'none';  // 日本語以外用広告
+        if (lang == 'en') {
+            // 言語切り替え
+            lang = 'ja';
+            document.getElementById('my-servers').style.display = 'block';
+            document.getElementById('header-text').textContent = "近接VC in Mcbe";
+            document.getElementById('name-disp').innerHTML = "ゲーマータグ: <span id='my-name'></span>";
+            document.getElementById('Menber-disp').innerHTML = "参加人数: <span id='Members'>不明</span>";
+            document.getElementById('address').textContent = "アドレス: ";
+            document.getElementById('gamertag').textContent = "ゲーマータグ: ";
+            document.getElementById('join').textContent = "接続する";
+            document.getElementById('NonMute-btn').textContent = "ミュートON/OFF";
+            document.getElementById('MuteInfo').textContent = "接続されていません";
+            document.getElementById('leave').textContent = "退出";
+            document.getElementById('participant').textContent = "参加者リスト";
+            document.getElementById('switch-language').textContent = "Switch to English";
+            if (isPC) {
+                document.getElementById('ad-japanese-pc').style.display = 'block';  // 日本語用広告
+                document.getElementById('ad-japanese2-pc').style.display = 'block';  // 日本語用広告
+                document.getElementById('ad-other-pc').style.display = 'none';  // 日本語以外用広告
+                document.getElementById('ad-other-pc2').style.display = 'none';  // 日本語以外用広告
+            } else {
+                document.getElementById('ad-japanese-phone').style.display = 'block';  // 日本語用広告
+                document.getElementById('ad-other-phone').style.display = 'none';  // 日本語以外用広告
+            }
         } else {
-            document.getElementById('ad-japanese-phone').style.display = 'block';  // 日本語用広告
-            document.getElementById('ad-other-phone').style.display = 'none';  // 日本語以外用広告
+            lang = 'en';
+            document.getElementById('my-servers').style.display = 'none';
+            document.getElementById('header-text').textContent = "Proximity VC in Mcbe";
+            document.getElementById('name-disp').innerHTML = "GamerTag: <span id='my-name'></span>";
+            document.getElementById('Menber-disp').innerHTML = "Participants: <span id='Members'>undefined</span>";
+            document.getElementById('address').textContent = "Address: ";
+            document.getElementById('gamertag').textContent = "GamerTag: ";
+            document.getElementById('join').textContent = "Connect";
+            document.getElementById('NonMute-btn').textContent = "Mute ON/OFF";
+            document.getElementById('MuteInfo').textContent = "Not connected";
+            document.getElementById('leave').textContent = "Leave";
+            document.getElementById('participant').textContent = "Participant List";
+            document.getElementById('switch-language').textContent = "日本語に切り替え";
+            if (isPC) {
+                document.getElementById('ad-japanese-pc').style.display = 'none';  // 日本語用広告
+                document.getElementById('ad-japanese2-pc').style.display = 'none';  // 日本語用広告
+                document.getElementById('ad-other-pc').style.display = 'block';  // 日本語以外用広告
+                document.getElementById('ad-other-pc2').style.display = 'block';  // 日本語以外用広告
+            } else {
+                document.getElementById('ad-japanese-phone').style.display = 'none';  // 日本語用広告
+                document.getElementById('ad-other-phone').style.display = 'block';  // 日本語以外用広告
+            }
         }
     });
-    const switchButton_ja = document.getElementById('switch-language-jp');
-    switchButton_ja.addEventListener('click', function () {
-        // 言語切り替え
-        lang = 'en';
-        document.getElementById('ja').style.display = 'none';
-        document.getElementById('en').style.display = 'block';
-        if (isPC) {
-            document.getElementById('ad-japanese-pc').style.display = 'none';  // 日本語用広告
-            document.getElementById('ad-japanese2-pc').style.display = 'none';  // 日本語用広告
-            document.getElementById('ad-other-pc').style.display = 'block';  // 日本語以外用広告
-            document.getElementById('ad-other-pc2').style.display = 'block';  // 日本語以外用広告
-        } else {
-            document.getElementById('ad-japanese-phone').style.display = 'none';  // 日本語用広告
-            document.getElementById('ad-other-phone').style.display = 'block';  // 日本語以外用広告
-        }
-    });
-
     // 広告の選択肢を定義
     const pc_yoko_ads = [
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CIP5PU+348+6OHEP" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CIP5PU+348+6OHEP" target="_blank" rel="nofollow">
 <img border="0" width="728" height="90" alt="" src="https://www27.a8.net/svt/bgt?aid=250122052757&wid=001&eno=01&mid=s00000000404001122000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=44WZES+CIP5PU+348+6OHEP" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CQFSKY+348+1C71F5" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CQFSKY+348+1C71F5" target="_blank" rel="nofollow">
 <img border="0" width="728" height="90" alt="" src="https://www27.a8.net/svt/bgt?aid=250122052770&wid=001&eno=01&mid=s00000000404008095000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=44WZES+CQFSKY+348+1C71F5" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CDXOVM+4EKC+62U35" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CDXOVM+4EKC+62U35" target="_blank" rel="nofollow">
 <img border="0" width="600" height="100" alt="" src="https://www23.a8.net/svt/bgt?aid=250122052749&wid=001&eno=01&mid=s00000020550001021000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=44WZES+CDXOVM+4EKC+62U35" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+64VU7M+50+7S05SX" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+64VU7M+50+7S05SX" target="_blank" rel="nofollow">
 <img border="0" width="728" height="90" alt="" src="https://www25.a8.net/svt/bgt?aid=250122056371&wid=001&eno=01&mid=s00000000018047030000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=44WZEW+64VU7M+50+7S05SX" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7A9VEP" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7A9VEP" target="_blank" rel="nofollow">
 <img border="0" width="728" height="90" alt="" src="https://www27.a8.net/svt/bgt?aid=250122056618&wid=001&eno=01&mid=s00000000018044052000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www18.a8.net/0.gif?a8mat=44WZEW+A7XWMQ+50+7A9VEP" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7A6FY9" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7A6FY9" target="_blank" rel="nofollow">
 <img border="0" width="728" height="90" alt="" src="https://www26.a8.net/svt/bgt?aid=250122056618&wid=001&eno=01&mid=s00000000018044036000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=44WZEW+A7XWMQ+50+7A6FY9" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7A9VEP" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7A9VEP" target="_blank" rel="nofollow">
 <img border="0" width="728" height="90" alt="" src="https://www24.a8.net/svt/bgt?aid=250122056618&wid=001&eno=01&mid=s00000000018044052000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=44WZEW+A7XWMQ+50+7A9VEP" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+ADAT2Q+CO4+2NA7NL" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+ADAT2Q+CO4+2NA7NL" target="_blank" rel="nofollow">
 <img border="0" width="468" height="60" alt="" src="https://www25.a8.net/svt/bgt?aid=250122056627&wid=001&eno=01&mid=s00000001642016004000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=44WZEW+ADAT2Q+CO4+2NA7NL" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+DCGTYQ+CO4+3H2J1T" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+DCGTYQ+CO4+3H2J1T" target="_blank" rel="nofollow">
 <img border="0" width="468" height="60" alt="" src="https://www24.a8.net/svt/bgt?aid=250122056807&wid=001&eno=01&mid=s00000001642021007000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www14.a8.net/0.gif?a8mat=44WZEW+DCGTYQ+CO4+3H2J1T" alt="">`,
         'adMax'
     ];
     const pc_tate_ads = [
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CIP5PU+348+6NU9D" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CIP5PU+348+6NU9D" target="_blank" rel="nofollow">
 <img border="0" width="160" height="600" alt="" src="https://www27.a8.net/svt/bgt?aid=250122052757&wid=001&eno=01&mid=s00000000404001119000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www15.a8.net/0.gif?a8mat=44WZES+CIP5PU+348+6NU9D" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CQFSKY+348+1C5BOX" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CQFSKY+348+1C5BOX" target="_blank" rel="nofollow">
 <img border="0" width="160" height="600" alt="" src="https://www26.a8.net/svt/bgt?aid=250122052770&wid=001&eno=01&mid=s00000000404008087000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www16.a8.net/0.gif?a8mat=44WZES+CQFSKY+348+1C5BOX" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+64VU7M+50+7RY8CX" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+64VU7M+50+7RY8CX" target="_blank" rel="nofollow">
 <img border="0" width="160" height="600" alt="" src="https://www29.a8.net/svt/bgt?aid=250122056371&wid=001&eno=01&mid=s00000000018047021000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=44WZEW+64VU7M+50+7RY8CX" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+79ZSR5" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+79ZSR5" target="_blank" rel="nofollow">
 <img border="0" width="160" height="600" alt="" src="https://www25.a8.net/svt/bgt?aid=250122056618&wid=001&eno=01&mid=s00000000018044005000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www13.a8.net/0.gif?a8mat=44WZEW+A7XWMQ+50+79ZSR5" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7AAQ9T" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+A7XWMQ+50+7AAQ9T" target="_blank" rel="nofollow">
 <img border="0" width="160" height="600" alt="" src="https://www28.a8.net/svt/bgt?aid=250122056618&wid=001&eno=01&mid=s00000000018044056000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www19.a8.net/0.gif?a8mat=44WZEW+A7XWMQ+50+7AAQ9T" alt="">`,
         'adMax'
     ];
 
     const pc_tate_ads2 = [
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CDXOVM+4EKC+639IP" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZES+CDXOVM+4EKC+639IP" target="_blank" rel="nofollow">
 <img border="0" width="300" height="250" alt="" src="https://www21.a8.net/svt/bgt?aid=250122052749&wid=001&eno=01&mid=s00000020550001023000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=44WZES+CDXOVM+4EKC+639IP" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEV+9DKSS2+461Y+61C2P" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEV+9DKSS2+461Y+61C2P" target="_blank" rel="nofollow">
 <img border="0" width="300" height="250" alt="" src="https://www27.a8.net/svt/bgt?aid=250122055567&wid=001&eno=01&mid=s00000019447001014000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www12.a8.net/0.gif?a8mat=44WZEV+9DKSS2+461Y+61C2P" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+64VU7M+50+7RY0N5" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+64VU7M+50+7RY0N5" target="_blank" rel="nofollow">
 <img border="0" width="300" height="250" alt="" src="https://www24.a8.net/svt/bgt?aid=250122056371&wid=001&eno=01&mid=s00000000018047020000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www17.a8.net/0.gif?a8mat=44WZEW+64VU7M+50+7RY0N5" alt="">`,
-        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+ADAT2Q+CO4+2NAN35" rel="nofollow">
+        `<a href="https://px.a8.net/svt/ejp?a8mat=44WZEW+ADAT2Q+CO4+2NAN35" target="_blank" rel="nofollow">
 <img border="0" width="300" height="250" alt="" src="https://www24.a8.net/svt/bgt?aid=250122056627&wid=001&eno=01&mid=s00000001642016006000&mc=1"></a>
 <img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=44WZEW+ADAT2Q+CO4+2NAN35" alt="">`,
         'adMax'
@@ -199,8 +222,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // 一定間隔で広告を変更（例: 5秒ごと）
     setInterval(updateAd, 20000);
 });
-
-
 
 async function fetchAppIdAndSecretId() {
     const socket = new WebSocket(url);
@@ -263,9 +284,8 @@ async function establishWebSocketConnection() {
 
 async function connectvc(userName) {
     try {
-        const address = document.getElementById('address-input' + (lang === 'ja' ? '-jp' : '')).value.trim();
         // WebSocketでapp_idとsecret_idを取得
-        const { app_id, secret_key } = await fetchAppIdAndSecretId(address);
+        const { app_id, secret_key } = await fetchAppIdAndSecretId();
 
         // Tokenの作成
         const Token = new SkyWayAuthToken({
@@ -324,15 +344,15 @@ async function SkyWay_main(token, userName) {
     const remoteMediaArea = document.getElementById('remote-media-area');
     const roomNameInput = "transceiver";
 
-    const myId = document.getElementById('my-id' + (lang === 'ja' ? '-jp' : ''));
-    const myName = document.getElementById('my-name' + (lang === 'ja' ? '-jp' : ''));
-    const Memberselem = document.getElementById('Members' + (lang === 'ja' ? '-jp' : ''));
-    const IdDisp = document.getElementById('id-disp' + (lang === 'ja' ? '-jp' : ''));
-    const joinButton = document.getElementById('join' + (lang === 'ja' ? '-jp' : ''));
-    const target = document.getElementById('MuteInfo' + (lang === 'ja' ? '-jp' : ''));
-    const NonMutebtn = document.getElementById('NonMute-btn' + (lang === 'ja' ? '-jp' : ''));
-    const leavebtn = document.getElementById('leave' + (lang === 'ja' ? '-jp' : ''));
-    const participantList = document.getElementById('participant-list' + (lang === 'ja' ? '-jp' : ''));
+    const myId = document.getElementById('my-id');
+    const myName = document.getElementById('my-name');
+    const Memberselem = document.getElementById('Members');
+    const IdDisp = document.getElementById('id-disp');
+    const joinButton = document.getElementById('join');
+    const target = document.getElementById('MuteInfo');
+    const NonMutebtn = document.getElementById('NonMute-btn');
+    const leavebtn = document.getElementById('leave');
+    const participantList = document.getElementById('participant-list');
 
     let isMuted = false;
 
@@ -607,10 +627,10 @@ async function SkyWay_main(token, userName) {
 
 // ページ読み込み時にボタンイベントハンドラを設定
 window.onload = async function () {
-    const joinButton = document.getElementById('join' + (lang === 'ja' ? '-jp' : ''));
+    const joinButton = document.getElementById('join');
     joinButton.onclick = async () => {
-        const userName = document.getElementById('user-name' + (lang === 'ja' ? '-jp' : '')).value.trim();
-        const address = document.getElementById('address-input' + (lang === 'ja' ? '-jp' : '')).value.trim();
+        const userName = document.getElementById('user-name').value.trim();
+        const address = document.getElementById('address-input').value.trim();
         url = `wss://${address}.tcpexposer.com`;
         if (userName === '') {
             if (lang === 'ja') {
